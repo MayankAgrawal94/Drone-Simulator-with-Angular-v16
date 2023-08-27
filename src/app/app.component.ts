ba2c77e2775b41440123159854781b21db5422fe
Mayank Agrawal <mayankagrawal4949@gmail.com>
2023-08-27 14:05:22 +0530

    Angular Google Maps demo test working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Drone-Simulator-with-Angular-v16';
+
+  G_API_KEY:string = "YOUR_KEY_HERE"
+
+  G_MAP_API:string = `https://maps.googleapis.com/maps/api/js?key=${this.G_API_KEY}`
+
+  apiLoaded: Observable<boolean>;
+
+  options: google.maps.MapOptions = {
+    center: {lat: 40, lng: -20},
+    zoom: 2
+  };
+
+  constructor(httpClient: HttpClient) {
+
+    this.apiLoaded = httpClient.jsonp(this.G_MAP_API, 'callback')
+        .pipe(
+          map(() => true),
+          catchError(() => of(false)),
+        );
+  }
 }
