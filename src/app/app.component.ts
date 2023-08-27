3ae52ee67f55879535863baea707b1e58d5d71f9
Mayank Agrawal <mayankagrawal4949@gmail.com>
2023-08-27 17:19:07 +0530

    Task: Drone Simulator completed
    Note: One Bonus Tasks also completed that provide user functionality to pause/resume/reset the simulation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,173 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, of, interval, Subscription } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-import { environment } from '../environments/environment.development';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Drone-Simulator-with-Angular-v16';
+export class AppComponent implements OnDestroy  {
+  title:string = "Drone Simulation"
 
   G_API_KEY:string = environment.G_API_KEY
 
   G_MAP_API:string = `https://maps.googleapis.com/maps/api/js?key=${this.G_API_KEY}`
 
+  MARKER_ICON:string = "assets/drone.png"
+
+  latLngForm:FormGroup;
+
+  private droneUpdateSubscription: Subscription = new Subscription();
+
   apiLoaded: Observable<boolean>;
 
-  center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
-  zoom = 1;
+  simulatePaused:boolean = false
+  
+  progressCount:number = 0;
+  numSteps:number = 100 // Number of intermediate positions
+  time:number = 0 // in milliseconds
 
-  vertices: google.maps.LatLngLiteral[] = [
-    {lat: 13, lng: 13},
-    {lat: -13, lng: 0},
-    {lat: 13, lng: -13},
-  ];
+  // Static start position for the Drone
+  positionA: google.maps.LatLngLiteral = {
+    lat: 28.522308592619723,
+    lng: 77.39657243970184
+  }
 
-  markerOptions: google.maps.MarkerOptions = {draggable: false};
-  markerPositions: google.maps.LatLngLiteral[] = [];
+  positionB: google.maps.LatLngLiteral = {
+    lat: 28.52387343192279,
+    lng: 77.39296719927432
+  }
 
-  constructor(httpClient: HttpClient) {
+  // Setting default map view
+  mainOptions: google.maps.MapOptions = {
+    center: this.positionA,
+    zoom: 2
+  };
 
-    this.apiLoaded = httpClient.jsonp(this.G_MAP_API, 'callback')
+  polylineOptions = {
+    visible: true, //Whether this polyline is visible on the map. @defaultValue - true
+    strokeColor: '#6d80b9c7',
+    // strokeOpacity: 4 ,// The stroke opacity between 0.0 and 1.0.
+    // strokeWeight: 4,  // The stroke width in pixels.
+    // path: [
+    //   {lat: 28.522308592619723, lng: 77.39657243970184},
+    //   {lat: 28.52387343192279, lng: 77.39296719927432},
+    // ]
+    // editable: false,
+    geodesic: true
+  }
+  polyVertices: google.maps.LatLngLiteral[] = [];
+
+  markerPositions: google.maps.LatLngLiteral = this.positionA
+  markerOption: google.maps.MarkerOptions = {
+    draggable: false,
+    // icon: "assets/drone.png",
+    label: 'S',
+    position: this.markerPositions,
+    // animation: google.maps.Animation.BOUNCE,
+  };
+  markerOptions: google.maps.MarkerOptions [] = [
+    this.markerOption
+  ]
+
+  constructor(httpClient: HttpClient,formBuilder: FormBuilder) {
+
+    this.apiLoaded = httpClient.jsonp(`${this.G_MAP_API}`, 'callback')
         .pipe(
           map(() => true),
           catchError(() => of(false)),
         );
+
+    this.latLngForm = formBuilder.group({
+      latitude: [null, [Validators.required]],
+      longitude: [null, [Validators.required]],
+      time: [null, [Validators.required,Validators.min(0)]],
+    });
   }
 
   addMarker(event: google.maps.MapMouseEvent) {
-    if(event && event.latLng)
-      this.markerPositions.push(event.latLng.toJSON());
+    // if(event && event.latLng)
+    //   console.log( "event =>", event, event.latLng.toJSON())
+  }
+
+  onSimulate(){
+    this.positionB = {
+      lat: this.latLngForm.value.latitude,
+      lng: this.latLngForm.value.longitude
+    }
+    this.time = (this.latLngForm.value.time * 1000) / 100
+
+    this.latLngForm.disable();
+
+    this.polyVertices = [
+      this.positionA,
+      this.positionB
+    ]
+
+    this.updateMarker(this.positionB.lat, this.positionB.lng, 2, false, 'E')
+
+    this.startDroneUpdates();
+  }
+
+  startDroneUpdates() {
+    this.droneUpdateSubscription = interval(this.time).subscribe(() => {
+      if( this.progressCount <= this.numSteps){
+        const fraction = this.progressCount / this.numSteps;
+        const intermediateLat = this.positionA.lat + fraction * (this.positionB.lat - this.positionA.lat);
+        const intermediateLng = this.positionA.lng + fraction * (this.positionB.lng - this.positionA.lng);
+        this.updateMarker(intermediateLat, intermediateLng, 1, true)
+        
+        this.progressCount++;
+      } else {
+        this.droneUpdateSubscription.unsubscribe();
+        this.progressCount--;   
+      }
+    });
+  }
+
+  updateMarker(
+    lat:number, lng:number, index:number, 
+      icon:boolean = false, lable:string = ''
+    ){
+      this.markerPositions = {lat,lng} 
+      this.markerOptions[index] = {
+          draggable: false,
+          icon: icon ? this.MARKER_ICON : '',
+          label: lable,
+          position: this.markerPositions,
+          zIndex: icon ? 1111 : 0
+      }
+  }
+
+  onPause(){
+    this.simulatePaused = true
+    this.droneUpdateSubscription.unsubscribe();
+  }
+
+  onResume(){
+    this.simulatePaused = false
+    this.startDroneUpdates();
+  }
+
+  onReset(){
+    this.simulatePaused = false
+    this.latLngForm.reset();
+    this.latLngForm.enable();
+    this.markerOptions[1] = {}
+    this.markerOptions[2] = {}
+    this.polyVertices = []
+    this.progressCount = 0
+    this.droneUpdateSubscription.unsubscribe();
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe from the subscription when the component is destroyed
+    if (this.droneUpdateSubscription) {
+      this.droneUpdateSubscription.unsubscribe();
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
-import { GoogleMapsModule } from '@angular/google-maps';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { GoogleMapsModule } from '@angular/google-maps';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,10 +23,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    GoogleMapsModule,
+    CommonModule,
     HttpClientModule,
     HttpClientJsonpModule,
-    GoogleMapsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
